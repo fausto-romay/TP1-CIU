7aa580d4ed74b4d7c8293cf117b6b29add8a8cb9
fausto-romay <fromay972@gmail.com>
2025-09-22 16:55:13 -0300

    Configuración de nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,19 @@ import logo from "../assets/logo2.png"
 function Header() {
     return (
         <header>
-            <img src={logo} alt="logo" className="header-logo" />
-            <h1 className="header-titulo">Luna & Granos Café</h1>
+            <div className="header-left">
+                <img src={logo} alt="logo" className="header-logo" />
+                <h1 className="header-titulo">Luna & Granos Café</h1>
+            </div>
+
+            <nav className="header-nav">
+                <ul>
+                    <li><a href="">INICIO</a></li>
+                    <li><a href="">CARTA</a></li>
+                    <li><a href="">CARRITO</a></li>
+                    <li><a href="">CONTACTO</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
