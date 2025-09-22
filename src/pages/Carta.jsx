1e1de973c46fe42ef62a85faf25d6920b0547162
fausto-romay <fromay972@gmail.com>
2025-09-22 17:21:39 -0300

    Alert

--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 function Carta() {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            alert("¡Hace click en el producto si queres sumarlo al carrito!");
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <h1>Página 2</h1>
     )
